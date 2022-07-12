a44b59a90280c7d8c915c3d32a95696744c1cdda
Arno Benizri <arno.benizri@gmail.com>
2022-07-12 21:30:09 +0300

    [UPDATE] README.md [UPDATE] Cleaner code for discogs scraping

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# webscraping_itc
-Datamining project where we will perform webscraping on Discogs and use Spotify's API to build a music themed database.
+# Datamining @ ITC
+Datamining and webscraping project where we will perform webscraping on Discogs and use Spotify's API to build a music themed database.
+
+## Packages used
+- BeautifulSoup4 (4.11.1)
+- spotipy (2.20.0)
+- Flask (2.1.2) ~ out of scope but fun embedding
+
+## How to use
+
+### Scraping Discogs
+
+Run `python discogs.py [count]`
+
+The parameter "count" is the number of pages to be scraped.
+
+This will display a list of Artist - Album scraped from the Discogs webpage.
+
+### Spotify queries
+
+- Run `python server.py` 
+- Go to http://127.0.0.1:5000/
+- Enter a Spotify search
--- discogs.py
+++ discogs.py
@@ -1,29 +1,60 @@
+#!/usr/bin/python3.9
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 
 from bs4 import BeautifulSoup
 import requests
+import sys
 
 
-ALBUMS_URL = "https://www.discogs.com/search/?sort=have%2Cdesc&ev=em_rs&type=master"
+BASE_URL = "https://www.discogs.com/search/?sort=have%2Cdesc&ev=em_rs&type=master"
 
-html_doc = requests.get(ALBUMS_URL)
-soup = BeautifulSoup(html_doc.text, features="html.parser")
+def print_results(artists, albums):
+    """
+    Prints the given artists - albums
+    Args:
+        artists: The artists list
+        albums: The album list
+    """
+    for artist, album in zip(artists, albums):
+        print(artist, "-", album)
 
-cards_layout = soup.find("div", {"class": "cards cards_layout_large"})
-soup_cards = BeautifulSoup(str(cards_layout), features="html.parser")
-cards = soup_cards.find_all("div", {"class": "card card_large float_fix shortcut_navigable"})
 
-artists, albums = [], []
-for card in cards:
-    album = card.find("a", {"class": "search_result_title"}).attrs["title"]
-    artist = card.find("span", attrs={'title': True}).attrs['title']
-    albums.append(album)
-    artists.append(artist)
-    
-print(artists)
-print(albums)
+def scrape_page(url):
+    """
+    Requests and scrapes the given discogs url
+    Args:
+        url: discogs url to scrape
+    """
+    html_doc = requests.get(url)
+    soup = BeautifulSoup(html_doc.text, features="html.parser")
+
+    cards_layout = soup.find("div", {"class": "cards cards_layout_large"})
+    soup_cards = BeautifulSoup(str(cards_layout), features="html.parser")
+    cards = soup_cards.find_all("div", {"class": "card card_large float_fix shortcut_navigable"})
+
+    artists, albums = [], []
+    for card in cards:
+        album = card.find("a", {"class": "search_result_title"}).attrs["title"]
+        artist = card.find("span", attrs={'title': True}).attrs['title']
+        albums.append(album)
+        artists.append(artist)
+
+    print_results(artists, albums)
+
+
+def main():
+    if len(sys.argv) == 1:
+        print("Scraping 3 pages by default")
+    elif len(sys.argv) > 2 or not sys.argv[1].isdigit():
+        print("usage: ./discogs.py count")
+        return
+
+    page_count = int(sys.argv[1])
+
+    for page in range(1, page_count + 1):
+        ALBUMS_URL = BASE_URL + f"&page={page}"
+        scrape_page(ALBUMS_URL)
+
+
+if __name__ == "__main__":
+    main()
