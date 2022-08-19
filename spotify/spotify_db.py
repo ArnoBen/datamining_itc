e83b6bcb55bca75907ccf80d86236aac493202be
Arno Benizri <arno.benizri@gmail.com>
2022-08-19 11:04:01 +0300

    [ADD] Request error handling

--- spotify/spotify_db.py
+++ spotify/spotify_db.py
@@ -1,3 +1,5 @@
+import logging
+
 from sql import DatabaseManager
 from spotify import SpotifyWrapper
 
@@ -6,21 +8,30 @@ class SpotifyDBFiller:
     def __init__(self):
         self.dbmanager = DatabaseManager()
         self.wrapper = SpotifyWrapper()
+        self.logger = logging.getLogger(__name__)
 
     def get_track_spotify_id(self, track_name: str, album: str, artist: str):
         """Search spotify to get the spotify's id of a given song"""
-        track_data = self.wrapper.search(f"{track_name} {album} {artist}")
-        if track_data:
-            track_id = track_data['id']
-            return track_id
-        else:
+        try:
+            track_data = self.wrapper.search(f"{track_name} {album} {artist}")
+            if track_data:
+                track_id = track_data['id']
+                return track_id
+            else:
+                return None
+        except ConnectionError as e:
+            self.logger.error("Spotify search failed with error:", e)
             return None
 
     def get_audio_features(self, ids: list):
         """Gets the audio features of a list of songs from spotify's api"""
         feature_names = ['danceability', 'energy', 'loudness', 'speechiness',
                          'acousticness', 'instrumentalness', 'valence', 'tempo']
-        return self.wrapper.get_audio_features(ids), feature_names
+        try:
+            return self.wrapper.get_audio_features(ids), feature_names
+        except ConnectionError as e:
+            self.logger.error("Spotify audio feature request failed with error:", e)
+            return None, None
 
     def fill_audio_features_in_db(self, db_ids: list, features: list):
         """Fill the tempo column of given tracks in the database"""
